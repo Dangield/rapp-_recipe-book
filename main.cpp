077891a0bcde6c2f16ebd2693b9ce23c2ccbd19d
rapp rapp <rapp-tygrys@tygrys.robotyka.ia.pw.edu.pl>
2016-05-21 12:47:13 +0200

    Dodane cyfy

--- main.cpp
+++ main.cpp
@@ -7,9 +7,40 @@
 
 std::vector<std::string> getRecipies(){
     std::vector<std::string> recipies;
-    recipies.push_back("pancake");
+    recipies.push_back("one");
+    recipies.push_back("two");
+    recipies.push_back("three");
+    recipies.push_back("four");
+    recipies.push_back("five");
 
     return recipies;
+}
+
+void tellRecipies(rapp::robot::communication comm)
+{
+    comm.text_to_speech("Say one for pancake.");
+}
+
+std::string getRecipeName(std::string result)
+{
+    std::string name;
+
+    if(result == "one")
+        name = "share/recipe-book/pancake.txt";
+    else if(result == "two")
+        name = "share/recipe-book/pancake.txt";
+    else if(result == "three")
+        name = "share/recipe-book/pancake.txt";
+    else if(result == "four")
+        name = "share/recipe-book/pancake.txt";
+    else if(result == "five")
+        name = "share/recipe-book/pancake.txt";
+    else if(result == "six")
+        name = "share/recipe-book/pancake.txt";
+    else if(result == "seven")
+        name = "share/recipe-book/pancake.txt";
+
+    return name;
 }
 
 
@@ -18,10 +49,11 @@ int main(int argc, char * argv[]) {
     rapp::robot::communication comm(argc, argv);
 
     comm.text_to_speech("What do you want to cook?");
+    tellRecipies(comm);
 
     // create list of words for recognition
     std::vector<std::string> words = getRecipies();
-    words.push_back("nothing");
+    words.push_back("abort");
     std::string result;
 
 
@@ -29,29 +61,32 @@ int main(int argc, char * argv[]) {
     do {
         result = comm.word_spotting(words);
 
-        if (result == "nothing") {
+        if (result == "abort") {
             comm.text_to_speech("Ok, Goodbye!");
             return 0;
         }
 
         else{
-            bool isUnderstand=false;
+            bool isUnderstand = false;
             for(int i = 0 ; i < words.size() ; i++)
                 if(words[i] == result){
-                    isUnderstand=true;
+                    isUnderstand = true;
                     break;
                 }
-            if(isUnderstand)break;
+            if(isUnderstand)
+                break;
         }
 
         comm.text_to_speech("Sorry, I don't understand you!");
 
     } while (1);
 
+    comm.text_to_speech(result);
+    result = getRecipeName(result);
+
     std::ifstream recipeFile;
-    result="share/recipe-book/"+result+".txt";
-    //const char* resultChar=result.c_str();
     recipeFile.open(info.get_path(result));
+
     if(!recipeFile.is_open()){
         comm.text_to_speech("Warning! Recipe missing!");
         return 0;
@@ -75,33 +110,33 @@ int main(int argc, char * argv[]) {
 
     comm.text_to_speech("Tell me what do you have?");
     do{
-       result = comm.word_spotting(words);
+        result = comm.word_spotting(words);
 
-       if(result == "all"){
-           break;
-       }
+        if(result == "all"){
+            break;
+        }
 
-       else if (result=="help"){
-           comm.text_to_speech("We need.");
-           for(int i = 0 ; i < words.size()-2 ; i++){
-               comm.text_to_speech(words[i]);
-           }
-       }
+        else if (result=="help"){
+            comm.text_to_speech("We need.");
+            for(int i = 0 ; i < words.size()-2 ; i++){
+                comm.text_to_speech(words[i]);
+            }
+        }
 
-       else{
-           bool isUnderstand=false;
-           for(int i = 0 ; i < words.size()-2 ; i++){
-               if(words[i] == result){
-                   words.erase(words.begin() + i);
-                   break;
-                   isUnderstand=true;
-               }
-           }
-           if(!isUnderstand)comm.text_to_speech("Sorry, I don't understand you!");
-       }
+        else{
+            bool isUnderstand=false;
+            for(int i = 0 ; i < words.size()-2 ; i++){
+                if(words[i] == result){
+                    words.erase(words.begin() + i);
+                    break;
+                    isUnderstand=true;
+                }
+            }
+            if(!isUnderstand)comm.text_to_speech("Sorry, I don't understand you!");
+        }
 
-       if(words.size() == 2)
-           break;
+        if(words.size() == 2)
+            break;
 
     }while(1);
 
@@ -120,9 +155,9 @@ int main(int argc, char * argv[]) {
         if(result=="repeat")continue;
         else if(result=="done"){
             std::getline(recipeFile, line);
-                if(line=="@")break;
+            if(line=="@")break;
         }
-            else comm.text_to_speech("Sorry, I don't understand you!");
+        else comm.text_to_speech("Sorry, I don't understand you!");
 
 
     }while(1);
